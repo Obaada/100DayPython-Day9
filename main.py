6f7fdfbd165b00bfabaef15956d6750cac0e6399
Obaada <159489903+Obaada@users.noreply.github.com>
2024-02-20 23:39:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+  "Loop": "The action of doing something over and over again." 
+}
+
+# Retrive items from a dictionary
+print(programming_dictionary["Loop"])
+
+# adding new items to the dictionary
+programming_dictionary["New"] = "This is an added item"
+
+# empty the dictionary 
+# programming_dictionary = {}
